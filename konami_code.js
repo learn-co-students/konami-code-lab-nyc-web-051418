26e3094facd729f204cad5cd9f7736bfa65eaa07
github username <github email address>
2018-06-30 19:15:33 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
+const keysTracker = []; 
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown',event => {
+    keysTracker.push(event.key)
+    if (keysTracker.length >= codes.length) {
+      l = keysTracker.length;
+      keysList = keysTracker.slice(l-codes.length,l);
+      // JS does not have a default method to compare arrays.
+      // A work-around is to stringify each array and compare.
+      if (JSON.stringify(keysList)===JSON.stringify(codes)) {
+        alert("Working!!!")
+      }
+    }
+  })
 }
